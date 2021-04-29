6c49241e02dd252bb05fd73ed887bf078229f94c
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2021-04-29 17:33:35 +0100

    update cartItem buttons/ create checkout file 1

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
-const Cart = ({cart}) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
-    <Typography variant='subtitle1'>You have no items in your shopping cart,
-      <Link to="/" className={classes.link}>Start adding some!</Link>
+    <Typography variant='subtitle1'>You have no items in your shopping cart, 
+      <Link to="/" className={classes.link}> Start adding some!</Link>
     </Typography>
   )
 
@@ -18,10 +18,11 @@ const Cart = ({cart}) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item}/>
+            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>
           </Grid>
         ))}
       </Grid>
+
       <div className={classes.cardDetails}>
         <Typography variant='h5'>
           Subtotal: { cart.subtotal.formatted_with_symbol}
@@ -33,10 +34,12 @@ const Cart = ({cart}) => {
             type='button' 
             variant='contained' 
             color='secondary'
+            onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
           <Button 
+            component={Link} to="/checkout"
             className={classes.checkoutButton} 
             size='large' 
             type='button' 
